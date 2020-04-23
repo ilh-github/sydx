04a7f2d2efeb3970706db75414e69ef164559eb7
ilh-github <53887385+ilh-github@users.noreply.github.com>
2020-04-23 17:31:13 +0800

    Update client.js

--- client.js
+++ client.js
@@ -5,6 +5,12 @@
           console.log("%c"+msg, "background:#66a6ff;padding:3px 5px;border-radius:10%;");
         }
         
+        
+        if(!$){
+            log("There is no jQuery, please try after refreshing");
+            return;
+        }
+        
         log("来自github...");
         console.log($("#app").attr("id"));
 
