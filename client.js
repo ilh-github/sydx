0f4d02ac0c530e4d50e34a1deb1a71306af7c2ab
ilh-github <53887385+ilh-github@users.noreply.github.com>
2020-04-23 17:26:13 +0800

    Update client.js

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
     (function () {
         'use strict';
+        
+        function log(msg) {
+          console.log(msg, "background:#66a6ff;padding:3px 5px;border-radius:10%;");
+        }
+        
         log("$c从github 注入脚本...");
         console.log($("#app").attr("id"));
 
