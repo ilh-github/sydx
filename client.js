31f8de92b42b81a68e644c76462db18814d34a5b
ilh-github <53887385+ilh-github@users.noreply.github.com>
2020-04-23 17:23:46 +0800

    Update client.js

--- client.js
+++ client.js
@@ -1,16 +1,6 @@
-(function () {
-    'use strict';
-    console.log("从github 注入脚本...");
-    // 解除复制，粘贴，剪切功能。
-    if (document.getElementById("app")) {
-        document.getElementById("app").removeAttribute("onpaste")
-        document.getElementById("app").removeAttribute("oncopy");
-        document.getElementById("app").removeAttribute("oncut");
+    (function () {
+        'use strict';
+        log("$c从github 注入脚本...");
+        console.log($("#app").attr("id"));
 
-        // 解除文字选中
-        const style = document.createElement("style");
-        style.innerHTML = "body :not(textarea) {user-select: auto!important;}";
-        document.head.appendChild(style);
-    }
-
-  })();
+    })();
